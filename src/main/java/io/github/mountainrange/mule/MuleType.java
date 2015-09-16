85948202e679890df29bcb33b0c3b58de0b2ee87
Allen Zheng <strburst@users.noreply.github.com>
2015-09-16 18:38:11 -0400

    Refactor/create MULE, resource, terrain enums

---
+++ src/main/java/io/github/mountainrange/mule/MuleType.java
@@ -0,0 +1,8 @@
+package io.github.mountainrange.mule;
+
+/**
+ * Describes the type of MULE installed on a given plot.
+ */
+public enum MuleType {
+	EMPTY, FOOD_MULE, ENERGY_MULE, CRYSTITE_MULE
+}
---
+++ src/main/java/io/github/mountainrange/mule/ResourceType.java
@@ -0,0 +1,8 @@
+package io.github.mountainrange.mule;
+
+/**
+ * Describe every resource in the game.
+ */
+public enum ResourceType {
+	FOOD, ENERGY, SMITHORE, CRYSTITE
+}
