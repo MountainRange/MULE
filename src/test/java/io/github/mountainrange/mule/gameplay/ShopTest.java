90273ca7bf588220e819399120154ab0bcbbbfe5
Jay Kamat <github@jgkamat.33mail.com>
2015-10-07 01:09:56 -0400

    Add coverage tests

---
+++ src/test/java/io/github/mountainrange/mule/gameplay/ShopTest.java
@@ -0,0 +1,16 @@
+package io.github.mountainrange.mule.gameplayer;
+
+// Junit Imports
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.*;
+
+
+/**
+ * A Class to Test the Shop
+ */
+public class ShopTest {
+
+
+
+}
